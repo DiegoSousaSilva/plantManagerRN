50420bd203f4e35ed1322a790493247d284d8ef1
DiegoSousaSilva <derickbenji2@gmail.com.br>
2022-10-11 22:37:04 -0300

    commit att

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,27 +1,31 @@
-import React, {useState} from 'react';
-import {Image, Text, SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react';
+import {
+  Image,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 
 import watering from '../assets/watering.png';
-import Button from '../components/Button';
 import colors from '../styles/colors';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Welcome: React.FC = () => {
-  const [visible, setVisible] = useState(true);
-
-  function handleVisibility() {
-    setVisible(!visible);
-  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
         Gerencie{'\n'} suas plantas{'\n'} de forma fácil
       </Text>
-      {visible && <Image source={watering} style={styles.image} />}
+      <Image source={watering} style={styles.image} resizeMode="contain" />
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-      <Button title="Avançar" onPress={handleVisibility} />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Icon name="chevron-thin-right" size={18} color="#FFF" />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -30,7 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -46,8 +50,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
 export default Welcome;
